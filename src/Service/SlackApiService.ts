16e5f3b2d06296fb89bc17312e137d6158272c58
shigematsu <shigematsu@snowrobin.jp>
2018-07-15 22:50:14 +0900

    Slackへの更新投稿処理の作成

--- src/Service/SlackApiService.ts
+++ src/Service/SlackApiService.ts
@@ -4,14 +4,22 @@ import { PostedMatch, PostedMatchId } from "../Entity/PostedMatch"
 
 interface PostMatchService {
   post(match: Match): Promise<PostedMatch>
-  update(postedMatchId: PostedMatchId, match: Match): Promise<void>
+  update(postedMatchId: PostedMatchId, match: Match): Promise<PostedMatch>
 }
 
-interface RequestBody {
+interface PostRequestBody {
   token: string
   channel: string
   username: string
   attachments: Array<{ fields: RequestBodyField[] }>
+}
+
+interface UpdateRequestBody {
+  token: string
+  channel: string
+  username: string
+  attachments: Array<{ fields: RequestBodyField[] }>
+  ts: string
 }
 
 interface RequestBodyField {
@@ -20,13 +28,92 @@ interface RequestBodyField {
   short: boolean
 }
 
+interface PostResponse {
+  ok: boolean
+  channel: string
+  ts: PostedMatchId
+  message: {
+    text: string
+    username: string
+    bot_id: string
+    attachments: any
+    type: string
+    subtype: string
+    ts: PostedMatchId
+  }
+}
+
 export class SlackApiPostMatchService implements PostMatchService {
-  private readonly requestUrl = "https://slack.com/api/chat.postMessage"
+  private readonly requestUrl = {
+    post: "https://slack.com/api/chat.postMessage",
+    update: "https://slack.com/api/chat.update",
+  }
   private readonly token = process.env.SLACK_TOKEN!
   private readonly channelNameId = process.env.SLACK_CHANNEL_NAME_ID!
 
+  /**
+   * 試合情報を投稿する
+   */
   async post(match: Match): Promise<PostedMatch> {
-    const requestJsonData: RequestBody = {
+    const requestJsonData: PostRequestBody = this.buildRequestBody(match)
+
+    const response = await this.request(this.requestUrl.post!, requestJsonData)
+
+    return {
+      postedMatchId: response.ts,
+      matchId: match.id,
+    }
+  }
+
+  /**
+   * 対象の postedMatchId (ts) の試合情報を更新する
+   * (Slack に通知済みの試合情報を最新の試合情報に更新)
+   *
+   * @param postedMatchId
+   * @param match
+   */
+  async update(postedMatchId: string, match: Match): Promise<PostedMatch> {
+    const requestJsonData: UpdateRequestBody = {
+      ...this.buildRequestBody(match),
+      ts: postedMatchId,
+    }
+
+    const response = await this.request(this.requestUrl.update!, requestJsonData)
+
+    return {
+      postedMatchId: response.ts,
+      matchId: match.id,
+    }
+  }
+
+  /**
+   * Slack Api への新規投稿/更新するリクエストを送信する
+   *
+   * @param url
+   * @param body
+   */
+  private async request(
+    url: string,
+    body: PostRequestBody | UpdateRequestBody
+  ): Promise<PostResponse> {
+    const response = await axios.post<PostResponse>(url, body, {
+      headers: {
+        Authorization: "Bearer " + this.token,
+      },
+    })
+
+    if (response.status === 200 && response.data.ok) {
+      return response.data
+    } else {
+      throw new Error("SlackAPIへのリクエストが失敗した")
+    }
+  }
+
+  /*
+   * RequestBody を生成する
+   */
+  private buildRequestBody = (match: Match): PostRequestBody => {
+    return {
       token: this.token,
       channel: this.channelNameId,
       username: match.getMatchName(),
@@ -52,34 +139,5 @@ export class SlackApiPostMatchService implements PostMatchService {
         },
       ],
     }
-
-    const response = await axios.post<PostResponse>(this.requestUrl!, requestJsonData, {
-      headers: {
-        Authorization: "Bearer " + this.token,
-      },
-    })
-    if (response.status === 200 && response.data.ok) {
-      return {
-        postedMatchId: response.data.ts,
-        matchId: match.id,
-      }
-    } else {
-      throw new Error("SlackAPIへのリクエストが失敗した")
-    }
-  }
-}
-
-interface PostResponse {
-  ok: boolean
-  channel: string
-  ts: PostedMatchId
-  message: {
-    text: string
-    username: string
-    bot_id: string
-    attachments: any
-    type: string
-    subtype: string
-    ts: PostedMatchId
   }
 }
